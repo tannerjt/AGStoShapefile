0383f3bc20cbd914ac32e9b053001f088113320a
tannerjt <biolsurfer85@gmail.com>
2018-01-15 13:05:30 -0800

    Convert to streaming

--- AGStoSHP.js
+++ AGStoSHP.js
@@ -7,22 +7,15 @@
 
 // Node Modules
 const fs = require('fs');
-const q = require('q');
-const request = q.nfbind(require('request'));
+const rp = require('request-promise');
+const request = require('request');
 const _ = require('lodash');
-const ogr2ogr = require('ogr2ogr');
 const TerraformerArcGIS = require('terraformer-arcgis-parser');
 const geojsonStream = require('geojson-stream');
 const JSONStream = require('JSONStream');
-
-const low = require('lowdb');
-const FileSync = require('lowdb/adapters/FileSync');
 const queryString = require('query-string');
-
-const datename = Date.now();
-const adapter = new FileSync(`./output/${datename}.json`);
-const db = low(adapter);
-
+const merge2 = require('merge2');
+const rimraf = require('rimraf');
 // ./mixin.js
 // merge user query params with default
 var mixin = require('./mixin');
@@ -33,10 +26,14 @@ if(outDir[outDir.length - 1] !== '/') {
 	outDir += '/';
 }
 
+// Remove trailing '/'
+outDir = outDir.replace(/\/$/, '');
+
 fs.readFile(serviceFile, function (err, data) {
 	if (err) {
 		throw err;
 	}
+
 	data.toString().split('\n').forEach(function (service) {
 		var service = service.split('|');
 		if(service[0].split('').length == 0) return;
@@ -52,16 +49,14 @@ fs.readFile(serviceFile, function (err, data) {
 		var qs = queryString.stringify(qs);
 		var url = decodeURIComponent(getBaseUrl(baseUrl) + '/query/?' + qs);
 
-		request({
+		rp({
 			url : url,
 			method : 'GET',
 			json : true
-		}, function (err, response, body) {
-			var err = err || body.error;
-			if(err) {
-				throw err;
-			}
+		}).then((body) => {
 			requestService(service[0].trim(), service[1].trim(), body.objectIds);
+		}).catch((err) => {
+		  console.log(err);
 		});
 	})
 });
@@ -71,21 +66,15 @@ function requestService(serviceUrl, serviceName, objectIds) {
 	objectIds.sort();
 	console.log('Number of features for service: ', objectIds.length);
 	console.log('Getting chunks of 100 features...');
-	var requests = [];
+	var requests = Math.ceil(objectIds.length / 100);
+	var completedRequests = 0;
 
-	for(var i = 0; i < Math.ceil(objectIds.length / 100); i++) {
+	for(let i = 0; i < Math.ceil(objectIds.length / 100); i++) {
 		var ids = [];
 		if ( ((i + 1) * 100) < objectIds.length ) {
 			ids = objectIds.slice(i * 100, (i * 100) + 100);
 		} else {
 			ids = objectIds.slice(i * 100, objectIds[objectIds.length]);
-		}
-
-		if(ids[0] !== undefined) {
-			console.log('query ->', (i * 100) , 'out of', objectIds.length);
-		} else {
-			console.log('wait for requests to settle...');
-			continue;
 		}
 
 		// we need these query params
@@ -104,74 +93,92 @@ function requestService(serviceUrl, serviceName, objectIds) {
 		var qs = mixin(userQS, reqQS);
 		var qs = queryString.stringify(qs);
 		var url = decodeURIComponent(getBaseUrl(serviceUrl) + '/query/?' + qs);
-		var r = request({
-			url : url,
-			method : 'GET',
-			json : true
-		});
 
-		requests.push(r);
-	};
 
-	q.allSettled(requests).then(function (results) {
-		console.log('all requests settled');
+		const partialsDir = `${outDir}/${serviceName}/partials`;
 
-		_.each(results, (result, idx) => {
-			if(!result || !result.value) {
-				console.log('server returned no result')
-				return
-			} else if (result.value[0].statusCode !== 200) {
-				console.warn('server returned no result', result.value[0].statusCode);
-				return;
-			} else if (!result.value[0].body || !result.value[0].body.features) {
-				console.warn('no result set returned');
-				return;
+		if(i == 0) {
+			if(!fs.existsSync(`${outDir}`)) {
+				fs.mkdirSync(`${outDir}`)
 			}
-			if(idx == 0) {
-				db.defaults(result.value[0].body)
-					.write();
+
+			if(!fs.existsSync(`${outDir}/${serviceName}`)) {
+				fs.mkdirSync(`${outDir}/${serviceName}`);
+			}
+
+			if (!fs.existsSync(partialsDir)){
+			    fs.mkdirSync(partialsDir);
 			} else {
-				db.get('features')
-					.push(...result.value[0].body.features)
-					.write();
+				rimraf.sync(partialsDir);
+				fs.mkdirSync(partialsDir);
 			}
-		});
+		}
 
 		const featureStream = JSONStream.parse('features.*', convert);
-		const outfile = fs.createWriteStream(`./output/${datename}.geojson`);
-		const infile = fs.createReadStream(`./output/${datename}.json`);
+		const outFile = fs.createWriteStream(`${partialsDir}/${i}.json`);
 
-		infile.on('end', () => {
-			buildShapefile();
-		});
+		const options = {
+			url: url,
+			method: 'GET',
+			json: true,
+		};
 
-		infile.pipe(featureStream)
+		request(options)
+			.pipe(featureStream)
 			.pipe(geojsonStream.stringify())
-			.pipe(outfile);
+			.pipe(outFile)
+			.on('finish', () => {
+				completedRequests += 1;
+				console.log(`Completed ${completedRequests} / ${requests} for ${serviceName}`);
+				if(requests == completedRequests) {
+					mergeFiles();
+				}
+			});
 
 		function convert (feature) {
 			const gj = {
-	  		  type: 'Feature',
-	  		  properties: feature.attributes,
-	  		  geometry: TerraformerArcGIS.parse(feature.geometry)
-	  		}
-	  		return gj
+				type: 'Feature',
+				properties: feature.attributes,
+				geometry: TerraformerArcGIS.parse(feature.geometry)
+			}
+			return gj
 		}
 
-		function buildShapefile () {
-			//shapefile
-			var shapefile = ogr2ogr(`./output/${datename}.geojson`)
-				.format('ESRI Shapefile')
-				.options(['-nln', datename])
-				.skipfailures()
-				.stream();
-			shapefile.pipe(fs.createWriteStream(`./output/${datename}.zip`));
+		function mergeFiles() {
+			fs.readdir(partialsDir, (err, files) => {
+				const finalFilePath = `${outDir}/${serviceName}/${serviceName}_${Date.now()}.geojson`
+				const finalFile = fs.createWriteStream(finalFilePath);
+
+				let streams = [];
+				_.each(files, (file) => {
+					streams.push(
+						fs.createReadStream(`${partialsDir}/${file}`)
+							.pipe(JSONStream.parse('features.*'))
+					)
+				});
+				merge2(streams)
+					.pipe(geojsonStream.stringify())
+					.pipe(finalFile)
+					.on('finish', () => {
+						rimraf(partialsDir, () => {
+							console.log(`${serviceName} is complete`);
+							console.log(`File Location: ${finalFilePath}`);
+						});
+					})
+			});
 		}
 
-	}).catch(function (err) {
-		console.log(err);
-		throw err;
-	});
+	};
+
+	// 	function buildShapefile () {
+	// 		//shapefile
+	// 		var shapefile = ogr2ogr(`./output/${datename}.geojson`)
+	// 			.format('ESRI Shapefile')
+	// 			.options(['-nln', datename])
+	// 			.skipfailures()
+	// 			.stream();
+	// 		shapefile.pipe(fs.createWriteStream(`./output/${datename}.zip`));
+	// 	}
 }
 
 
